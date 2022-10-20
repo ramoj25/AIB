cf4e8ce1d055ee901a58340ef965169b54bc689b
NKC25 <96770025+NKC25@users.noreply.github.com>
2022-10-20 22:34:53 +0200

    Update existingSD.ps1

--- existingSD.ps1
+++ existingSD.ps1
@@ -37,22 +37,3 @@ catch {
 
 Write-host 'AIB Customization: EndRegion IBM'
 #endregion IBM a2
-
-
-
-
-#install IBM
-Write-host 'AIB Customization: Install IBM'
-try {
-   Start-Process -filepath msiexec.exe -Wait -ErrorAction Stop -ArgumentList "/i","`"C:\apps\AVD_SD_Apps1\IBM\a3\source\IBM Personal Communications.msi`"","TRANSFORMS=`"C:\apps\AVD_SD_Apps1\IBM\a3\source\IBM_Personnal_Communications_13.0.0.mst`"","/qn","/l*v","c:\windows\temp\ibm.log"
-    }
-catch {
-    $ErrorMessage = $_.Exception.message
-    
-    write-host "Error IBM a3: $ErrorMessage"
-}
-
-Write-host 'AIB Customization: EndRegion IBM'
-#endregion IBM a3
-
-
