1633830533989efb5b441efe35812125d6a50e30
ramoj25 <30527561+ramoj25@users.noreply.github.com>
2023-12-13 16:47:18 +0100

    Update carweaver.ps1
    update careweaver

--- carweaver.ps1
+++ carweaver.ps1
@@ -1,14 +1,14 @@
 #install AIP
-Write-host 'AIB Customization: Install AIP'
+Write-host 'AIB Customization: Install CarWeaver'
 try {
-    Start-Process -filepath "C:\apps\AVDapps\AIP\Deploy-Application.exe" -Wait -ErrorAction Stop 
-    write-log "AIP installed successfully"
-    write-host "AIP installed successfully"
+    Start-Process -filepath "C:\apps\Systemite CarWeaver Production\Systemite CarWeaver Production\Deploy-Application.exe" -Wait -ErrorAction Stop 
+    write-log "CarWeaver installed successfully"
+    write-host "CarWeaver installed successfully"
     }
 catch {
     $ErrorMessage = $_.Exception.message
-    write-log "Error installing AIP: $ErrorMessage"
-    Write-host "Error installing AIP: $ErrorMessage"
+    write-log "Error installing CarWeaver: $ErrorMessage"
+    Write-host "Error installing CarWeaver: $ErrorMessage"
 }
 #endregion
-Write-host 'AIB Customization: EndRegion AIP'
+Write-host 'AIB Customization: EndRegion CarWeaver'
